1cad718ba0b48fab601721f5072c26f8aa07795b
Stephanie Ahlström <ahlstrom.stephanie.se@gmail.com>
2021-04-02 14:20:08 +0200

    Initial commit

---
+++ spiral/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Spiral :count="6" />
+</template>
+
+<script>
+import Spiral from "./components/Spiral.vue";
+
+export default {
+  name: "App",
+  components: {
+    Spiral,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ spiral/src/components/Spiral.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="spiral">
+    <h1>{{ count }}</h1>
+    <div id="matrix">
+      <div class="row" v-for="row in numbers" :key="row.index">
+        <div class="number" v-for="number in row" :key="number.index">
+          {{ number }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Spiral",
+  props: {
+    count: Number,
+  },
+  data() {
+    return {
+      numbers: this.renderMatrix(),
+    };
+  },
+  methods: {
+    renderMatrix() {
+      const matrix = new Array();
+      let direction = "right";
+
+      for (let j = 0; j < this.count; ++j) {
+        const row = new Array();
+        for (let i = 0; i < this.count; ++i) {
+          row[i] = null;
+        }
+        matrix.push(row);
+      }
+
+      let x = 0,
+        y = 0;
+
+      matrix[0][0] = 1;
+
+      for (let i = 1; i < this.count * this.count; ++i) {
+        if (direction == "right") {
+          if (x + 1 < this.count && !matrix[y][x + 1]) {
+            x++;
+          } else {
+            direction = "down";
+          }
+        }
+        if (direction == "down") {
+          if (y + 1 < this.count && !matrix[y + 1][x]) {
+            y++;
+          } else {
+            direction = "left";
+          }
+        }
+        if (direction == "left") {
+          if (x - 1 >= 0 && !matrix[y][x - 1]) {
+            x--;
+          } else {
+            direction = "up";
+          }
+        }
+        if (direction == "up") {
+          if (y - 1 >= 0 && !matrix[y - 1][x]) {
+            y--;
+          } else {
+            direction = "right";
+            x++;
+          }
+        }
+        console.log("x: " + x + ", y: " + y);
+        matrix[y][x] = i + 1;
+      }
+      return matrix;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+#matrix {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.row {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.number {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 2em;
+  height: 2em;
+  border: 1px #000 solid;
+}
+</style>
